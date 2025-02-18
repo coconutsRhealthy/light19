a68064989456e2fd46fccbd20bffaa8227df5cbd
lennartmac <lennartpopma@hotmail.com>
2025-02-18 08:43:52 +0100

    First working base version, with base contact component

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./../app.component.css']
+})
+export class ContactComponent implements OnInit {
+
+  isMenuCollapsed = true;
+
+  flameEmoji = "\u{1F525}";
+  influencerEmoji = "\u{1F4F7}";
+  companyEmoji = "\u{1F3E2}";
+  eyesEmoji = "\u{1F440}";
+
+  constructor() {
+    //this.meta.updateTitle("Neem Contact Op met Diski | Kortingscodes en Samenwerkingen");
+    //this.meta.updateMetaInfo("Duizenden online shoppers maken gebruik van de kortingscodes die Diski deelt. Heb je vragen, verzoeken of ideeën voor samenwerkingen of adverteren? Neem contact op en ontdek hoe we samen kunnen werken via Diski.nl en @wiegeeftkorting.", "diski.nl", "Samenwerken met Diski, Adverteren op kortingscode platform, Kortingscodes voor online shoppers, Samenwerkingen met bedrijven");
+  }
+
+  ngOnInit(): void {
+  }
+
+}
