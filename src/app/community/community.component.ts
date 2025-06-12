e1f292782cb5a93b7a9f13fc61d7ce2d3bdcf4be
lennartmac <lennartpopma@hotmail.com>
2025-06-12 21:26:18 +0200

    Render the community table nicely

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -10,12 +10,38 @@ import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 export class CommunityComponent implements OnInit {
 
   webhookUrl = 'https://script.google.com/macros/s/AKfycbwtzvmRMxK8GklpS_z3W8DvPd9sNRAKazy8FPRC0k80cgKYAEQ_Sny073hUH7rp8fgK/exec';
-  rows: any[] = [];
+  rows: string[][] = [];
+  headers: string[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.readDataFromSheet();
+  }
+
+  readDataFromSheet() {
+    const url = 'https://docs.google.com/spreadsheets/d/1TsBlQr5KrB3g6rbtwr0ujLoFfi41XBwBgimMCyQBl9w/gviz/tq?tqx=out:json';
+
+    this.http.get(url, { responseType: 'text' }).subscribe(response => {
+      const json = JSON.parse(response.replace(/^[^\(]*\(/, '').replace(/\);$/, ''));
+      const table = json.table;
+
+      this.headers = table.cols.map((col: any) => col.label);
+      this.rows = table.rows.map((row: any) =>
+        row.c.map((cell: any) => {
+          const raw = cell?.v ?? '';
+
+          const dateMatch = typeof raw === 'string' && raw.match(/^Date\((\d+),(\d+),(\d+)\)$/);
+          if (dateMatch) {
+            const [_, year, month, day] = dateMatch;
+            const jsDate = new Date(+year, +month, +day);
+            return jsDate.toLocaleDateString('nl-NL');
+          }
+
+          return raw;
+        })
+      );
+    });
   }
 
   addRow() {
@@ -35,13 +61,5 @@ export class CommunityComponent implements OnInit {
       });
   }
 
-  readDataFromSheet() {
-    const url = 'https://docs.google.com/spreadsheets/d/1TsBlQr5KrB3g6rbtwr0ujLoFfi41XBwBgimMCyQBl9w/gviz/tq?tqx=out:json';
 
-    this.http.get(url, { responseType: 'text' }).subscribe(response => {
-      const json = JSON.parse(response.replace(/^[^\(]*\(/, '').replace(/\);$/, ''));
-      const table = json.table;
-      this.rows = table.rows.map((row: any) => row.c.map((cell: any) => cell?.v ?? ''));
-    });
-  }
 }
