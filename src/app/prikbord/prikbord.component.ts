029bc93831c833aae32145f2fd1613fefa493e5f
lennartmac <lennartpopma@hotmail.com>
2025-07-22 11:47:25 +0200

    Cleanup of prikbord component, including renaming whs logic to prikbord

--- src/app/prikbord/prikbord.component.ts
+++ src/app/prikbord/prikbord.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MetaService } from '../services/meta.service';
+import { AnalyticsEventService } from '../services/analytics-event.service';
 import { FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { PrikbordModalComponent } from '../prikbord-modal/prikbord-modal.component';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 
-import { AnalyticsEventService } from '../services/analytics-event.service';
-
-import { PrikbordModalComponent } from '../prikbord-modal/prikbord-modal.component';
 
 declare global {
   interface Window {
@@ -15,7 +14,7 @@ declare global {
   }
 }
 
-interface WhsEntry {
+interface PrikbordEntry {
   webshop: string;
   code: string;
   percentage: number;
@@ -27,17 +26,19 @@ interface WhsEntry {
   selector: 'app-prikbord',
   imports: [PrikbordModalComponent, FooterComponent, NavbarComponent, FormsModule],
   templateUrl: './prikbord.component.html',
-  styleUrls: ['./prikbord.component.css', './../app.component.css']
+  styleUrls: ['./../app.component.css']
 })
 export class PrikbordComponent implements OnInit {
-  wieheeftsaleData: WhsEntry[] = [];
-  filteredWieheeftsaleData: WhsEntry[] = [];
+  prikbordData: PrikbordEntry[] = [];
+  filteredprikbordData: PrikbordEntry[] = [];
   searchTerm: string = '';
   page: number = 1;
   itemsPerPage: number = 50;
   sortByCompanyAscending = false;
   sortByDateAscending = false;
   sendCopyCodeToGa = window.sendCopyCodeToGa;
+  webhookUrl = 'https://script.google.com/macros/s/AKfycbzEMMokt67Oz0PwOHEKHwxyZLpw0rwfVyzCXnerdNSwrxf4pKX6pz9_-KX48APoe_AX/exec';
+  modalVisible = false;
 
   constructor(private meta: MetaService, private http: HttpClient, private analyticsEventService: AnalyticsEventService) {
     var monthYear = this.meta.getDateString();
@@ -57,7 +58,7 @@ export class PrikbordComponent implements OnInit {
       const json = JSON.parse(response.replace(/^[^\(]*\(/, '').replace(/\);$/, ''));
       const table = json.table;
 
-      this.wieheeftsaleData = table.rows.map((row: any) => {
+      this.prikbordData = table.rows.map((row: any) => {
         const cells = row.c.map((cell: any) => cell?.v ?? '');
         const [webshop, code, percentage, date, added_by] = cells;
 
@@ -67,23 +68,45 @@ export class PrikbordComponent implements OnInit {
           percentage: +percentage,
           date,
           added_by,
-        } as WhsEntry;
+        } as PrikbordEntry;
       }).reverse();
-      this.filteredWieheeftsaleData = this.wieheeftsaleData;
+      this.filteredprikbordData = this.prikbordData;
     });
   }
 
+  onCodeAdded(newCode: any) {
+    console.log('Nieuwe kortingscode:', newCode);
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
+
+    const body = new HttpParams()
+      .set('webshop', newCode.webshop)
+      .set('code', newCode.code)
+      .set('percentage', newCode.percentage)
+      .set('date', newCode.date)
+      .set('name', newCode.name);
+
+    this.http.post(this.webhookUrl, body.toString(), { headers, responseType: 'text' })
+      .subscribe({
+        next: () => {},
+        error: (err) => {
+          console.error(err);
+        },
+      });
+  }
+
   onSearch() {
-    this.filteredWieheeftsaleData = this.wieheeftsaleData.filter((whsEntry) =>
-      whsEntry.webshop.toLowerCase().includes(this.searchTerm.toLowerCase())
+    this.filteredprikbordData = this.prikbordData.filter((PrikbordEntry) =>
+      PrikbordEntry.webshop.toLowerCase().includes(this.searchTerm.toLowerCase())
     );
     this.page = 1;
   }
 
-  get paginatedWhsEntries(): WhsEntry[] {
+  get paginatedPrikbordEntries(): PrikbordEntry[] {
     const start = (this.page - 1) * this.itemsPerPage;
     const end = start + this.itemsPerPage;
-    return this.filteredWieheeftsaleData.slice(start, end);
+    return this.filteredprikbordData.slice(start, end);
   }
 
   nextPage() {
@@ -95,60 +118,23 @@ export class PrikbordComponent implements OnInit {
   }
 
   get totalPages(): number {
-    return Math.ceil(this.filteredWieheeftsaleData.length / this.itemsPerPage);
+    return Math.ceil(this.filteredprikbordData.length / this.itemsPerPage);
   }
 
   sortByCompany() {
     this.sortByCompanyAscending = !this.sortByCompanyAscending;
-    this.filteredWieheeftsaleData.sort((a, b) => {
+    this.filteredprikbordData.sort((a, b) => {
       const comparison = a.webshop.localeCompare(b.webshop);
       return this.sortByCompanyAscending ? comparison : -comparison;
     });
   }
 
-  openUrlInNewTab(url: string) {
-    window.open(url, '_blank');
-  }
-
   sendEventToGa(eventName: string, eventLabel: string): void {
-    var eventLabelToUse = "whs_company_click_" + eventLabel.toLowerCase();
+    var eventLabelToUse = "prikbord_" + eventLabel.toLowerCase();
     this.analyticsEventService.sendEventToGa(eventName, eventLabelToUse);
   }
 
-
-  //////
-
-
-  webhookUrl = 'https://script.google.com/macros/s/AKfycbzEMMokt67Oz0PwOHEKHwxyZLpw0rwfVyzCXnerdNSwrxf4pKX6pz9_-KX48APoe_AX/exec';
-  modalVisible = false;
-
   showModal() {
-      this.modalVisible = true;
-    }
-
-    onCodeAdded(newCode: any) {
-      console.log('Nieuwe kortingscode:', newCode);
-
-      // Voeg hier je logica toe om de nieuwe kortingscode op te slaan, bijv. in array of server
-
-      const headers = new HttpHeaders({
-        'Content-Type': 'application/x-www-form-urlencoded',
-      });
-
-      const body = new HttpParams()
-        .set('webshop', newCode.webshop)
-        .set('code', newCode.code)
-        .set('percentage', newCode.percentage)
-        .set('date', newCode.date)
-        .set('name', newCode.name);
-
-      this.http.post(this.webhookUrl, body.toString(), { headers, responseType: 'text' })
-        .subscribe({
-          next: () => {},
-          error: (err) => {
-            console.error(err);
-          },
-        });
-    }
-
+    this.modalVisible = true;
+  }
 }
