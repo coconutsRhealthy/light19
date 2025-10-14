fc4b3d75507699bd3750aac1266aa20ab7ed4081
lennartmac <lennartpopma@hotmail.com>
2025-10-14 11:18:00 +1100

    Giftcard component (v2.0) - second version including radiobutton sorting

--- src/app/giftcards/giftcards.component.ts
+++ src/app/giftcards/giftcards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { DiscountsService } from '../services/discounts.service';
 
 interface Discount {
@@ -11,20 +12,22 @@ interface Discount {
 
 @Component({
   selector: 'app-giftcards',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './giftcards.component.html',
   styleUrls: ['./../app.component.css', './../app-tailwind.component.css']
 })
 export class GiftcardsComponent implements OnInit {
   discounts: Discount[] = [];
+  originalOrder: Discount[] = [];
   loading = true;
+  sortMode: 'popular' | 'alphabetical' = 'popular';
 
   constructor(private discountsService: DiscountsService) {}
 
   ngOnInit() {
     this.discountsService.getDiscounts().subscribe({
       next: (data) => {
-        this.discounts = data
+        const parsed = data
           .map((line: string, index: number) => {
             const [company, discountCode, percentage, , date] = line
               .split(',')
@@ -33,6 +36,8 @@ export class GiftcardsComponent implements OnInit {
           })
           .filter((d) => d.discountCode.startsWith('http'));
 
+        this.discounts = [...parsed];
+        this.originalOrder = [...parsed];
         this.loading = false;
       },
       error: (err) => {
@@ -40,5 +45,17 @@ export class GiftcardsComponent implements OnInit {
         this.loading = false;
       },
     });
+  }
+
+  onSortChange(mode: 'popular' | 'alphabetical'): void {
+    this.sortMode = mode;
+
+    if (mode === 'alphabetical') {
+      this.discounts = [...this.discounts].sort((a, b) =>
+        a.company.localeCompare(b.company)
+      );
+    } else {
+      this.discounts = [...this.originalOrder];
+    }
   }
 }
