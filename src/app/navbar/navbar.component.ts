6552339fe7d6e3b07b000a01ca7f321e213c86de
lennartmac <lennartpopma@hotmail.com>
2025-10-21 13:32:40 +1100

    Navbar correct (real) names and routing, and navbar active

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
