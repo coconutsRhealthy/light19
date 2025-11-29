e42ba5bb61b8f2db8d6245af6bb4222d38168e0e
lennartmac <lennartpopma@hotmail.com>
2025-11-29 19:42:08 +0100

    SSG3: fixes to make company pages work prerendered

--- src/app/services/discounts.service.ts
+++ src/app/services/discounts.service.ts
@@ -1,22 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
+// Statische JSON importeren
+import discountsData from './discounts_static_test.json';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DiscountsService {
-  private manifestUrl = '/manifest.json';
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
+  // Huidige getDiscounts() vervangt HTTP-call door statische data
   getDiscounts(): Observable<string[]> {
-    const timestamp = new Date().getTime();
-    const cacheBustedUrl = `${this.manifestUrl}?cache-bust=${timestamp}`;
-
-    return this.http.get<{ latestDiscounts: string }>(cacheBustedUrl).pipe(
-      switchMap(manifest => this.http.get<string[]>(`/${manifest.latestDiscounts}`))
-    );
+    return of(discountsData);
   }
 }
