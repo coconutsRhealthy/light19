f6df1414825ab07f9170ce1ac8fe77e46d5f001b
lennartmac <lennartpopma@hotmail.com>
2025-12-15 16:23:37 +1100

    SSG3: some extra non SSG development work: code updates, banner update etc, small fixes

--- scripts/fill-routes.js
+++ scripts/fill-routes.js
@@ -29,7 +29,7 @@ json.forEach((line) => {
 });
 
 // --- Add hardcoded routes ---
-const hardcodedRoutes = ['/giftcards', '/winkels', '/prikbord', '/wieheeftsale', '/'];
+const hardcodedRoutes = ['/giftcards', '/winkels', '/prikbord', '/wieheeftsale', '/top5', '/'];
 hardcodedRoutes.forEach(route => results.add(route.replace(/^\//, '')));
 
 // Sort alphabetically
