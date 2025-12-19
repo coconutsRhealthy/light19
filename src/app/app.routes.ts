5e67a6094cbdd096d31ab5a7ec377c47897abe2b
lennartmac <lennartpopma@hotmail.com>
2025-12-19 11:41:02 +1100

    SSG3: no lazy loading to fix MIME errors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,46 +1,24 @@
 import { Routes } from '@angular/router';
 import { DiscountsTableComponent } from './discounts-table/discounts-table.component';
 import { CompanyCodesComponent } from './company-codes/company-codes.component';
+import { WinkelsComponent } from './winkels/winkels.component';
+import { ContactComponent } from './contact/contact.component';
+import { WieheeftsaleComponent } from './wieheeftsale/wieheeftsale.component';
+import { PrikbordComponent } from './prikbord/prikbord.component';
+import { GiftcardsComponent } from './giftcards/giftcards.component';
+import { AmbassadorComponent } from './ambassador/ambassador.component';
+import { Top5Component } from './top5/top5.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: DiscountsTableComponent
-    },
-    {
-        path: 'winkels',
-        loadComponent: () => import('./winkels/winkels.component').then(m => m.WinkelsComponent)
-    },
-    {
-        path: 'contact',
-        loadComponent: () => import('./contact/contact.component').then(m => m.ContactComponent)
-    },
-    {
-        path: 'wieheeftsale',
-        loadComponent: () => import('./wieheeftsale/wieheeftsale.component').then(m => m.WieheeftsaleComponent)
-    },
-    {
-        path: 'prikbord',
-        loadComponent: () => import('./prikbord/prikbord.component').then(m => m.PrikbordComponent)
-    },
-    {
-        path: 'giftcards',
-        loadComponent: () => import('./giftcards/giftcards.component').then(m => m.GiftcardsComponent)
-    },
-    {
-        path: 'ambassador',
-        loadComponent: () => import('./ambassador/ambassador.component').then(m => m.AmbassadorComponent)
-    },
-    {
-        path: 'top5',
-        loadComponent: () => import('./top5/top5.component').then(m => m.Top5Component)
-    },
-    {
-        path: ':company',
-        component: CompanyCodesComponent
-    },
-    {
-        path: '**',
-        loadComponent: () => import('./not-found/not-found.component').then(m => m.NotFoundComponent)
-    }
+  { path: '', component: DiscountsTableComponent },
+  { path: 'winkels', component: WinkelsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'wieheeftsale', component: WieheeftsaleComponent },
+  { path: 'prikbord', component: PrikbordComponent },
+  { path: 'giftcards', component: GiftcardsComponent },
+  { path: 'ambassador', component: AmbassadorComponent },
+  { path: 'top5', component: Top5Component },
+  { path: ':company', component: CompanyCodesComponent },
+  { path: '**', component: NotFoundComponent }
 ];
